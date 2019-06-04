8b66175c63b6ea48148672d14ed7ee326b4e6e48
puppypaduk2 <debraooclark@gmail.com>
2019-06-04 19:06:24 +0300

    create template with only one file in template

--- src/core/commands/create.js
+++ src/core/commands/create.js
@@ -1,22 +1,31 @@
 const program = require('commander');
 const ora = require('ora');
+const fs = require('fs');
+const pt = require('path');
 
 const { remove, create, clone } = require('../common/shell');
 
 const action = (nameTemplate, { path = '.', templateConfig }) => {
   const sampiConfig = require(program.config);
   const templatePath = `${sampiConfig.paths.templates}/${nameTemplate}`;
-  const indicator = ora('Created template').start();
+  const indicator = ora('Create template').start();
+  const cbSucceed = () => indicator.succeed('Create template!');
 
   remove(templatePath, () => {
-    create(templatePath, () => {
-      clone(path, `${templatePath}/template`, () => {
-        if (templateConfig) {
-          clone(templateConfig, `${templatePath}/config.js`, () => {
-            indicator.succeed('Create template!');
-          });
+    let pathTo = `${templatePath}/template`;
+
+    create(pathTo, () => {
+      if (fs.statSync(path).isDirectory() === false) {
+        pathTo = `${pathTo}/${pt.basename(path)}`;
+      }
+
+      clone(path, pathTo, (err) => {
+        if (err) {
+          indicator.fail(err.message);
+        } else if (templateConfig) {
+          clone(templateConfig, `${templatePath}/config.js`, cbSucceed);
         } else {
-          indicator.succeed('Create template!');
+          cbSucceed();
         }
       });
     });
